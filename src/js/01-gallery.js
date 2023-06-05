a005c1771873276a07e492ad4c9efcabddac5418
Vova-Kalbenko <lastforhs3@gmail.com>
2023-06-05 13:47:10 +0300

    init-02

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galeryEl = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+ </li>`
+  )
+  .join("");
+
+galeryEl.insertAdjacentHTML("beforeend", markup);
+
+const handleClickOnImg = (e) => {
+  e.preventDefault();
+};
+
+galeryEl.addEventListener("click", handleClickOnImg);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionPosition: "bottom",
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+console.log(lightbox);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,23 @@
+import throttle from 'lodash.throttle';
+
+const formEl = document.querySelector('.feedback-form');
+const messageValue = 'feedback-form-state';
+
+let formData = {};
+
+const saveMessage = e => {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem('messageValue', JSON.stringify(formData));
+  console.log(e.target.name)
+  console.log(e.target.value)
+};
+
+  if(localStorage.getItem('messageValue')){
+  formData = JSON.parse(localStorage.getItem('messageValue'))
+  for(let key in formData){
+    formEl.elements[key].value = formData[key]
+  }
+}
+
+formEl.addEventListener('submit', throttle(saveMessage, 500));
 
