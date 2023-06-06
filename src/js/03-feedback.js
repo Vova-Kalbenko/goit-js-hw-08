7ef35bc12c50ce462e1f65e40494a06fcb175613
Vova-Kalbenko <lastforhs3@gmail.com>
2023-06-06 12:30:10 +0300

    fix-task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,20 +2,32 @@ import throttle from 'lodash.throttle';
 
 const formEl = document.querySelector('.feedback-form');
 const messageValue = 'feedback-form-state';
-
+const buttonEL = document.querySelector('[type="submit"]');
 let formData = {};
-
+const { email, message } = formEl.elements;
 const saveMessage = e => {
   formData[e.target.name] = e.target.value;
   localStorage.setItem('messageValue', JSON.stringify(formData));
 };
 
-  if(localStorage.getItem('messageValue')){
-  formData = JSON.parse(localStorage.getItem('messageValue'))
-  for(let key in formData){
-    formEl.elements[key].value = formData[key]
+if (localStorage.getItem('messageValue')) {
+  formData = JSON.parse(localStorage.getItem('messageValue'));
+  for (let key in formData) {
+    formEl.elements[key].value = formData[key];
   }
 }
 
+const handlesubmitForm = e => {
+  e.preventDefault();
+  console.log({ email: email.value, message: message.value });
+  email.value === '' || message.value === ''
+
+  localStorage.removeItem('messageValue');
+  e.currentTarget.reset();
+
+};
+
 formEl.addEventListener('input', throttle(saveMessage, 500));
+formEl.addEventListener('submit', handlesubmitForm);
+
 
